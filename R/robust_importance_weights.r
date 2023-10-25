f5aa935241c161990f005195b140d5c417e392e4
Ming Yuan <ymyuan@student.ubc.ca>
2023-10-25 15:44:38 -0700

    Update robust_importance_weights.r

--- R/robust_importance_weights.r
+++ R/robust_importance_weights.r
@@ -34,6 +34,12 @@
 #' By default, \eqn{\text{bisquare_tuning_k} = 4.685 \times \hat{\sigma}}
 #' and \eqn{\hat{\sigma}} is estimated using the same equation above.
 #'
+#' @param previous_imp_weights importance weights in the previous iteration,
+#' used to calculate the importance weights in the current iteration for the
+#' S-estimation / MM-estimation. If \param{previous_imp_weights} is NULL,
+#' it indicates the first iteration, and thus for S-estimation and MM-estimation
+#' need to be initialized.
+#'
 #'
 #' @return {imp_weights} The imp weights.
 #' If a subject is to be removed from the current iteration, its adjuct_weights
@@ -45,13 +51,16 @@
 robust_importance_weights <- function(
     values,
     robust_method = c("none", "huber", "simple", "bisquare"),
-    simple_outlier_fraction = 0.01,
+    simple_outlier_fraction = NULL,
     simple_outlier_thres = NULL,
-    huber_tuning_k = NULL,
-    bisquare_tuning_k = NULL
+    # huber_tuning_k = NULL,
+    huber_tuning_k = c("M", "S", "vSD", "vMd"),
+    bisquare_tuning_k = NULL,
+    previous_imp_weights = NULL  # if is NULL, S-estimation needs to be initialized.
 ){
 
   robust_method <- match.arg(robust_method)
+  huber_tuning_k <- match.arg(huber_tuning_k)
 
   if (robust_method == "none") {
     imp_weights <- rep(1, times = length(values))
@@ -59,32 +68,58 @@ robust_importance_weights <- function(
   }
   else if (robust_method == "simple") {
     if (!is.null(simple_outlier_fraction)) {
-      # message(paste("fraction", simple_outlier_fraction))
       if (simple_outlier_fraction <= 0 | simple_outlier_fraction >= 1)
-        {stop("simple_outlier_fraction should be a value between 0 and 1")}
+      {stop("simple_outlier_fraction should be a value between 0 and 1")}
+
       thres <- quantile(values, 1 - simple_outlier_fraction)
       imp_weights <- 1 * (values <= thres)
-      message(paste(which(values > thres), collapse = " "))
     }
     else if(!is.null(simple_outlier_thres)) { # is.null(simple_outlier_fraction)
-      # message(paste("fixed threshold", simple_outlier_thres))
       thres <- simple_outlier_thres
       imp_weights <- 1 * (values <= thres)
-
     }
     else { # is.null(simple_outlier_fraction) && is.null(simple_outlier_thres)
       stop("Please specify a threshold to simple-robust estimation!")
     }
   }
   else if (robust_method == "huber"){
-    # message("huber weighting")
-    if (is.null(huber_tuning_k)) {
-      huber_tuning_k <- 1.345 * median(abs(values)) / 0.6745
+    # value: residual
+
+    # if (is.null(huber_tuning_k)) {
+    if (huber_tuning_k == "M") {  # M-Estimation
+      hat_sigma_r <- median(abs(values - median(values))) / 0.6745
+      huber_tuning_k <- 1.345 * hat_sigma_r
+      # super conservative
+      imp_weights <- huber_weight_(values, huber_tuning_k)
     }
-    imp_weights <- huber_weight_(values, huber_tuning_k)
+    else if (huber_tuning_k == "S") { # S-Estimation
+      if (is.null(previous_imp_weights)) {
+        # iteration = 1, initialization
+        hat_sigma_r <- median(abs(values - median(values))) / 0.6745
+        huber_tuning_k <- 1.345 * hat_sigma_r
+        imp_weights <- huber_weight_(values, huber_tuning_k)
+
+      } else { # for iteration > 1
+        hat_sigma_r <- sqrt(
+          sum(sweep(previous_imp_weights, 1, values^2, "*")) /
+            (length(values) * 0.199)
+          )
+        sdr <- values / hat_sigma_r  # standardized residuals u
+        imp_weights <- huber_loss_(sdr, 1.345) / sdr^2
+      }
+    }
+    else if(huber_tuning_k == "vSD") {
+      huber_tuning_k <- 1.345 * sd(values)
+      imp_weights <- huber_weight_(values, huber_tuning_k)
+
+    }
+    else if (huber_tuning_k == "vMd") {
+      huber_tuning_k <- 1.345 * median(abs(values))
+      imp_weights <- huber_weight_(values, huber_tuning_k)
+    }
+
 
   } else if (robust_method == "bisquare") {
-    # message("bisquare weighting")
     if (is.null(bisquare_tuning_k)) {
       bisquare_tuning_k <- 4.685 * median(abs(values)) / 0.6745
     }
@@ -94,6 +129,11 @@ robust_importance_weights <- function(
 
   return(imp_weights)
 
+}
+
+huber_loss_ <- function(r, k) {
+  out <- ifelse(abs(r) <= k, r^2/2, k*abs(r) - k^2/2)
+  return(out)
 }
 
 huber_weight_ <- function(r, k) {
