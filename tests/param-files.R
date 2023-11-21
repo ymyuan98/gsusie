974b6fc5e99c87ac08514e2a316f1989b12bcc4f
Ming Yuan <ymyuan@student.ubc.ca>
2023-11-20 22:37:18 -0800

    update

--- tests/param-files.R
+++ tests/param-files.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 ####################################################################
 ## Parameters settings for synthetic (all-random) data
 
@@ -20,6 +22,36 @@ write.table(param_comb, file = "./tests/params.txt",
 # save as `params.txt` in the remote host.
 
 
+##################
+## Synthetic settings 2
+
+nn <- c(500, 800)
+pp <- c(1000, 3000, 5000)
+h2 <- c(0.01, 0.1, 0.2, 0.4)
+n_effect_vars <- c(5, 3, 1)
+
+h2_2 <- 0.3
+
+
+param_comb <- as.data.frame(
+  rbind(expand.grid(n = nn, p = pp, h2 = h2, n_effect_vars = n_effect_vars),
+        expand.grid(n = nn, p = pp, h2 = h2_2, n_effect_vars = n_effect_vars)
+        )) %>%
+  mutate(ID = 1 : n()) %>%
+  select(ID, n, p, h2, n_effect_vars)
+  slice(rep(1 : n(), each = 100)) %>% # (repeat each simulation 100 times)
+  mutate(seed = 1 : n()) %>%
+  select(ID, seed, n, p, h2, n_effect_vars)
+
+
+write.csv(param_comb, file = "./tests/params-synthetic.csv",
+            row.names = F, col.names = F)
+# save as `params.txt` in the remote host.
+
+## Additional 18 cases: ID 73-90
+## ID 73-90 (row 7201 - 9000), h2 = 0.3
+
+
 ####################################################################
 ## Parameters settings for genomic data
 
@@ -28,20 +60,27 @@ pp <- c(2000, 5000, 10000)
 h2 <- c(0.01, 0.1, 0.3)
 n_effect_vars <- c(1, 3, 5)
 
-param_comb <- as.data.frame(expand.grid(n = nn, p = pp, h2 = h2,
-                                        n_effect_vars = n_effect_vars)) %>%
+h2_2 <- c(0.2, 0.4) ## additional
+
+param_comb <- as.data.frame(
+  rbind(expand.grid(n = nn, p = pp, h2 = h2, n_effect_vars = n_effect_vars),
+        expand.grid(n = nn, p = pp, h2 = h2_2, n_effect_vars = n_effect_vars)
+        )) %>%
   mutate(ID = 1 : n()) %>%
+  select(ID, n, p, h2, n_effect_vars)
   slice(rep(1 : n(), each = 100)) %>% # (repeat each simulation 100 times)
   mutate(seed = 1 : n()) %>%
   select(ID, seed, n, p, h2, n_effect_vars)
-
-param_comb %>%
-  filter(ID == 54)
 
 ## 54 cases in total, repeat each simulation 100 times
 ## ID 1-18,  (row 1-1800),    n_effect_vars <- 1
 ## ID 19-36, (row 1801-3600), n_effect_vars <- 3
 ## ID 37-54, (row 3601-5400), n_effect_vars <- 5
 
-write.table(param_comb, file = "./tests/params-genom.txt",
-          row.names = F,col.names = F)
+## Additional 36 cases: ID 55-90
+## ID 55-60, 67-72, 79-84, h2 = 0.2
+## ID 61-66, 73-78, 85-90, h2 = 0.4
+
+
+write.csv(param_comb, file = "./tests/params-genotype.csv",
+          row.names = F)
